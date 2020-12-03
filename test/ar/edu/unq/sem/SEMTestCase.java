f577375d7fdb7b8079bbb4c60d9465c862b91d93
Juan Pablo Díaz <juampi00.est@gmail.com>
2020-12-03 17:45:56 -0300

    Agregado del mockito

--- test/ar/edu/unq/sem/SEMTestCase.java
+++ test/ar/edu/unq/sem/SEMTestCase.java
@@ -1,13 +1,21 @@
 package ar.edu.unq.sem;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SEMTestCase {
+	private SistemaDeAsociaciones sa;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		sa = mock(SistemaDeAsociaciones.class);
+	}
 	
 	@Test
 	public void testasdasd() {
-		assertEquals(1, 1);
+		assertEquals(sa, sa);
 	}
 }
