12a3bfb873fa084206aa96d9dd97475bd8fef1c4
Leandro Tittarelli <leandrotittarelli@gmail.com>
2020-12-03 13:52:22 -0300

    Modificado el punto de venta.

--- src/ar/edu/unq/zona/PuntoDeVenta.java
+++ src/ar/edu/unq/zona/PuntoDeVenta.java
@@ -2,21 +2,26 @@ package ar.edu.unq.zona;
 
 import java.time.LocalTime;
 
+import ar.edu.unq.compras.CompraPuntual;
 import ar.edu.unq.estacionamiento.EstacionamientoCompraPuntual;
 import ar.edu.unq.sem.SEM;
 
 public class PuntoDeVenta {
+	
+	private Zona zonaPDV;
+	
+	public PuntoDeVenta(Zona zonaPDV) {
+		this.zonaPDV = zonaPDV;
+	}
 	
 	public void recargarCelular(Long nroCelular, Double credito) {
 		SEM.getSEM().getSistemaDeSaldos().recargarSaldo(nroCelular, credito);
 	}
 	
 	public void iniciarEstacionamiento(String patente, Integer horas) {
-		this.validarHora(horas);
-		SEM.getSEM().agregarEstacionamiento(new EstacionamientoCompraPuntual(patente, horas)); // Modificar Singleton con el SEM;
+		CompraPuntual compra =  new CompraPuntual(SEM.getSEM().siguienteNC(), this, horas);
+		SEM.getSEM().agregarEstacionamiento(new EstacionamientoCompraPuntual(patente, compra));
+		SEM.getSEM().agregarCompra(compra);
 	}
-	
-	private void validarHora(Integer horas) {
-		//TODO
-	}
+
 }
