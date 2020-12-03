608e3153677f93d98fa4dd6cc158bf8965c5aadb
DavidIgnacioDuarte <davidignacio20@outlook.com>
2020-12-03 02:44:21 -0300

    Cerrados protocolos de Estacionamientos.

--- src/ar/edu/unq/compras/CompraPuntual.java
+++ src/ar/edu/unq/compras/CompraPuntual.java
@@ -7,19 +7,19 @@ import ar.edu.unq.zona.PuntoDeVenta;
 
 public class CompraPuntual extends Compra {
 
-	private double horasCompradas;
+	private Integer horasCompradas;
 	
 	
-	public CompraPuntual(Integer numeroC, LocalTime horaEfectuada, LocalDate fechaEfectuada, PuntoDeVenta puntoDeVenta, double cantidadHorasCompradas) {
+	public CompraPuntual(Integer numeroC, LocalTime horaEfectuada, LocalDate fechaEfectuada, PuntoDeVenta puntoDeVenta, Integer cantidadHorasCompradas) {
 		super(numeroC, horaEfectuada, fechaEfectuada, puntoDeVenta);
 		this.setHorasCompradas(cantidadHorasCompradas);
 	}
 	
-	public double getHorasCompradas() {
+	public Integer getHorasCompradas() {
 		return horasCompradas;
 	}
 	
-	public void setHorasCompradas(double horasCompradas) {
+	public void setHorasCompradas(Integer horasCompradas) {
 		this.horasCompradas = horasCompradas;
 	}
 	
